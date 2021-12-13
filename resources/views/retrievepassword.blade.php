30e39a3137199109eddbd7c6c16634d1d2f3fa53
Diego Muñoz Herranz <diego171200@gmail.com>
2021-12-13 20:22:32 +0100

    Change Mail Format and Fix Middleware

--- resources/views/retrievepassword.blade.php
+++ resources/views/retrievepassword.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Retrieve Password</title>
-    </head>
-    <body>
-        <h2>{{$title}}</h2>
-       <p>Tu nueva contraseña: {{$data}}</p>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recuperar Contraseña</title>
+</head>
+<body>
+    <h2>{{$title}}</h2>
+    <p>Tu nueva contraseña: {{$data}}</p>
+    
+    <p><i>Le recomendamos que modifique su contraseña lo antes posible para evitar otra perdida.</i></p>
+
+    <span style="text-align:center">&copy; Diego Muñoz Herranz 2021</span>
+</body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware(['api_auth', 'check_user_profile'])->group(function () {
     Route::get('/employee_list', [UserController::class, 'employee_list']);
     Route::get('/employee_detail', [UserController::class, 'employee_detail']);
     Route::get('/see_profile', [UserController::class, 'see_profile'])->withoutMiddleware('check_user_profile');
-    Route::put('/retrieve_password', [UserController::class, 'retrieve_password'])->withoutMiddleware('check_user_profile');
+    Route::put('/retrieve_password', [UserController::class, 'retrieve_password'])->withoutMiddleware(['api_auth', 'check_user_profile']);
     Route::post('/modify_data', [UserController::class, 'modify_data']);
 });
