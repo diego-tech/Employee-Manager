27c303a8cc8e87a6c801b4101f6963e8e89f0ef2
Diego <diego171200@gmail.com>
2022-02-09 17:30:52 +0100

    Comment User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,22 +18,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $workplace_array = ['Directivo','RRHH','Empleado'];
+        /* Seeder de Pruebas de Usuarios en Local */
 
-        for ($i = 1; $i < 20; $i++) {
-            DB::table('users')->insert([
-                'name' => Str::random(20),
-                'email' => Str::random(10) . '@gmail.com',
-                'password' => Hash::make('password'),
-                'workplace' => Arr::random($workplace_array),
-                'salary' => Str::random(10) . '$',
-                'biography' => Str::random(200),
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ]);
-        }
+        // $workplace_array = ['Directivo','RRHH','Empleado'];
+
+        // for ($i = 1; $i < 20; $i++) {
+        //     DB::table('users')->insert([
+        //         'name' => Str::random(20),
+        //         'email' => Str::random(10) . '@gmail.com',
+        //         'password' => Hash::make('password'),
+        //         'workplace' => Arr::random($workplace_array),
+        //         'salary' => Str::random(10) . '$',
+        //         'biography' => Str::random(200),
+        //         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        //         'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        //     ]);
+        // }
 
 
+        /* Generación de un usario administrador para comenzar con la generación de usuarios */
         DB::table('users')->insert([
             'name' => "Admin",
             'email' => "admin@admin.admin",
